761547e3c243a45c5a0d4efca67f2379007dbd3a
Aiades <1026203551@qq.com>
2025-11-04 14:06:16 +0800

    修改basename

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/ai-digital-collab-mainxx">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/demo" element={<AIDemoPage />} />
